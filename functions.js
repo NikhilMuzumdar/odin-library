fa93f9976cf6d68ae1699a0cd1f6e2b5053c11a3
Nikhil <nikhil.neon@gmail.com>
2023-01-28 23:48:54 +0530

    updated skeliton files

---
+++ functions.js
@@ -0,0 +1,15 @@
+let myLibrary = [];
+
+function Book(book, author, pages, read) {
+  // the constructor...
+  this.book = book;
+  this.author = author;
+  this.pages = pages;
+}
+
+function addBookToLibrary() {
+  // do stuff here
+  
+
+}
+
